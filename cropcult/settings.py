79d23d4f796f9243627e7144924c123e400a0636
Harry Wykman <harry@blackearthcollective.org.au>
2016-03-06 13:26:37 +0800

    foundation

--- cropcult/settings.py
+++ cropcult/settings.py
@@ -230,6 +230,8 @@ if DJANGO_VERSION < (1, 9):
 ################
 
 INSTALLED_APPS = (
+    "data_garden_theme",
+    "debug_toolbar",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
